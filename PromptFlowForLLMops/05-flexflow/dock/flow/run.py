4c6b841e1b3f52ab613521a22217dd06e32b8e18
Citizen-XX <thlorient@gmail.com>
2024-10-17 17:15:06 -0600

    'DockerStuffDidntWork'

---
+++ PromptFlowForLLMops/05-flexflow/dock/flow/run.py
@@ -0,0 +1,33 @@
+import os
+from promptflow.tracing import start_trace, trace
+from dotenv import load_dotenv
+from promptflow.core import Prompty, AzureOpenAIModelConfiguration
+
+#starting the trace on our python chat function
+@trace
+def start_chat_with_model(input: str) -> str:
+    #loading the .env file
+    load_dotenv()
+    #creating the AzureOpenAIModelConfiguration object
+    model_config = AzureOpenAIModelConfiguration(
+        azure_endpoint=os.getenv("OPENAI_ENDPOINT"),
+        api_key=os.getenv("OPENAI_KEY"),
+        azure_deployment=os.getenv("OPENAI_DEPLOYMENT_NAME"),
+        api_version="2024-05-01-preview"
+    )
+
+    #creating the Prompty object
+    prompty_file_path = r'chat.prompty'
+    # prompty_file_path = r'PromptFlowForLLMops\05-flexflow\chat.prompty'
+    prompty = Prompty.load(prompty_file_path, model={'configuration': model_config})
+
+    #prompting the user with the chat history and chat input
+    result=prompty(chat_input=input)
+    
+    #displaying the response
+    return result
+
+if __name__ == "__main__":
+    start_trace()
+    result = start_chat_with_model("Hello, how are you?")
+    print(result)
--- PromptFlowForLLMops/05-flexflow/run.py
+++ PromptFlowForLLMops/05-flexflow/run.py
@@ -1,7 +1,5 @@
 import os
 from promptflow.tracing import start_trace, trace
-import dotenv
-from dotenv import load_dotenv
 from dotenv import load_dotenv
 from promptflow.core import Prompty, AzureOpenAIModelConfiguration
 
@@ -12,17 +10,24 @@ def start_chat_with_model(input: str) -> str:
     load_dotenv()
     #creating the AzureOpenAIModelConfiguration object
     model_config = AzureOpenAIModelConfiguration(
-        azure_endpoint=os.getenv("OPENAI_ENDPOINT"),
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
         api_key=os.getenv("OPENAI_KEY"),
         azure_deployment=os.getenv("OPENAI_DEPLOYMENT_NAME"),
         api_version="2024-05-01-preview"
     )
 
     #creating the Prompty object
-    prompty = Prompty.load("chat.prompty", model={'configuration': model_config})
+    prompty_file_path = r'chat.prompty'
+    # prompty_file_path = r'PromptFlowForLLMops\05-flexflow\chat.prompty'
+    prompty = Prompty.load(prompty_file_path, model={'configuration': model_config})
 
     #prompting the user with the chat history and chat input
     result=prompty(chat_input=input)
     
     #displaying the response
     return result
+
+if __name__ == "__main__":
+    start_trace()
+    result = start_chat_with_model("Hello, how are you?")
+    print(result)
